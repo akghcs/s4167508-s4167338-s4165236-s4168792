27b560e41de1e8d1db1f1a8c943866e381f940c9
s4165236 <s4165236@student.rmit.edu.au>
2026-08-04 19:14:07 +1000

    progress on enemy

---
+++ Scenes/enemy.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed: float = 100
+@export var direction: Vector2 = Vector2.DOWN
+
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	velocity = direction.normalized() * speed
+	look_at(position + velocity)
+	move_and_collide(velocity * delta)

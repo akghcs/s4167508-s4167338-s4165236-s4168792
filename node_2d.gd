448e55b695a06bb57e2cab963cdef6cd02b46485
s4165236 <s4165236@student.rmit.edu.au>
2026-08-07 20:27:27 +1000

    enemy now spawns

--- node_2d.gd
+++ node_2d.gd
@@ -1,8 +1,21 @@
 extends Node2D
-
+@export var enemy_to_spawn: PackedScene 
+@onready var shapeSize = $EnemySpawner/CollisionShape2D.shape.extents
+@onready var origin = $EnemySpawner/CollisionShape2D.global_position
+@onready var startSpawn = origin - shapeSize
+@onready var endSpawn = origin + shapeSize
 @onready var parallex_background: ParallaxBackground = $ParallaxBackground
 
 const SCROLL_SPEED : float = -150.0
 
 func _process(delta: float):
 	parallex_background.scroll_offset.y -= delta * SCROLL_SPEED
+
+
+
+func _on_spawn_timer_timeout():
+	var x = randf_range(startSpawn.x, endSpawn.x)
+	var y = randf_range(startSpawn.y,endSpawn.y)
+	var e = enemy_to_spawn.instantiate()
+	e.position = Vector2(x,y)
+	call_deferred("add_child", e)
